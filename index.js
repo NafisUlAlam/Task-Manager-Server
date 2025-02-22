eb2f55daea44e7c6cbc5874f03d36c3c9bfea24f
Nafis <ahmednafis74@gmail.com>
2025-02-22 17:34:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("server is falling");
+});
+
+app.listen(port, () => {
+  console.log("server is running at port", port);
+});
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@freecluster.9tzex.mongodb.net/?retryWrites=true&w=majority&appName=FreeCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+
+    const taskCollection = client.db("task-managerDB").collection("tasks");
+
+    app.post("/tasks", async (req, res) => {
+      const data = req.body;
+      //console.log(data);
+      const result = await taskCollection.insertOne(data);
+      res.send(result);
+    });
+    app.get("/tasks", async (req, res) => {
+      //console.log(req.query);
+      const query = req.query;
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
